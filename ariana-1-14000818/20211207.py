a030bb5997214bc713d0609944d1fa29855a7389
hooman <hoomanjavan74@gmail.com>
2021-12-29 21:11:45 +0330

    add ariana1 class codes to project

---
+++ ariana-1-14000818/20211207.py
@@ -0,0 +1,47 @@
+# print('09304857238')
+# print(4567 + 6456)
+
+# print('hello'
+#       'world')
+
+# print("""hello
+# world""")
+
+# print(2 + 2)
+#
+# print(abs(-10))
+
+# x = pow(2, 3)
+# print(x + 10)
+# a = 6
+# b = 7
+# c = a + b
+# print(c)
+
+# age = input("enter your age: ")
+
+# print('hello ', age)
+# print(type(age))
+
+# print('09304857238')
+# print('hello world')
+# a = "hello"
+# print(a)
+
+# age = input('enter your age: ')
+# print("your age is: ", int(age) + 10)
+# a = "hello"
+# print(a)
+
+a = input('enter your first number: ')
+b = input('enter your second number: ')
+print('your result is: ',  int(a) + int(b))
+
+"Hi my name is *NAME*, and I am *AGE* years old."
+age = input("Enter age: ")
+name = input("Enter name: ")
+print("Hi, my name is " + name + ", and I am " + age + " years old.")
+
+
+
+
---
+++ ariana-1-14000818/test.py
@@ -0,0 +1,2 @@
+name= input('test')
+print(name)
