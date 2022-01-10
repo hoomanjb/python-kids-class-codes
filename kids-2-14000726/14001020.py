6d270d7bbdb603234774612067094fb956c869e5
hooman <hoomanjavan74@gmail.com>
2022-01-10 18:57:20 +0330

    class-14001020 file

---
+++ kids-2-14000726/14001020.py
@@ -0,0 +1,71 @@
+# first_name = 'mahya'
+# a = first_name.count('m')
+#
+# b = 'print'
+# c = print
+#
+# print('a')
+# print(a)
+
+# name = "hooman"
+# my_list = [2, 4, 6, 7, 8]
+#
+# for item in my_list:
+#     print(item)
+
+
+
+# name = input('enter your name: ')
+# sub = input('enter your char for find: ')
+# counter = 0
+# for item in name:
+#     if item == sub:
+#         counter += 1
+#
+# print('your counter char is: ', counter)
+
+# name = input('enter your name: ')
+
+# for item in name:
+#     if item == 'z':
+#         print('warning')
+
+# find_z = 0
+# index = 0
+# while find_z == 0:
+#     if name[index] == 'z':
+#         find_z = 1
+#         print('warning')
+#     index += 1
+
+
+# name = 'hooman'
+# print(name[3])
+
+# break
+
+# name = input('enter your name: ')
+
+# for item in name:
+#     if item == 'z':
+#         print('warning')
+#         break
+
+# continue
+# for item in range(1, 1000):
+#     if (item % 2) != 0:
+#         print('adad zoj nis')
+#     else:
+#         print(item)
+
+# for item in range(1, 1000):
+#     if (item % 2) != 0:
+#         print('adad zoj nis')
+#     print(item)
+
+name = 'hooma20n'
+
+print(name.isalpha())
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+if 'item' in a:
+    print(' item is number')
