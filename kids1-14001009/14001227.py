63a3d927fc4d0fea6b0e7b71a1b7bec2f157e73a
hoomanjb <hoomanjavan74@gmail.com>
2022-03-19 17:46:11 +0330

    add kids1 new file

---
+++ kids1-14001009/14001227.py
@@ -0,0 +1,50 @@
+# Animal Quiz
+# 5ta soal tarahi shavad va az karbar porside shavad
+# masalan mesle bozorgtarin heyvan khoshki? ya darya?
+# agar karbar doros javab dad yek emtiaz b oo bedahid dar gheyre in soorat soale baadi porside shavad
+# dar akhar emtiaz b kar bar namayesh dade shavad
+
+# emtiaz = 0
+# user_input = input('1- natije amaliate moghabel che mishavad? 123 + 78 : ')
+# print(user_input)
+# emtiaz += 1
+#
+# print('2- javabe dorost ra entekhab konid: 123 + 78  ')
+# print('7')
+# print('9')
+# user_input = input()
+# print('emtiaz shoma: ' + emtiaz)
+
+# ###################################################
+# Nine Lives
+
+# Apple
+# Lives = 9
+# ['?', '?', '?', '?', '?']
+
+# Lives = 9
+# ['?', 'p', 'p', '?', '?']
+
+# Lives = 8
+# ['?', 'p', 'p', '?', '?']
+
+# Lives = 7
+# ['?', 'p', 'p', '?', '?']
+
+# Lives = 7
+# ['a', 'p', 'p', 'l', 'e']
+
+#print('shoma moavafagh shodi kalameye morede nazar apple bood')
+
+#print('shoma moavafagh nashodi kalameye morede nazar apple bood')
+
+# ####################################################
+
+# Password Picker
+# تعداد کارکترهای پسورد چنتا باشد؟ بله)0 خیر)1
+# آیا از اعداد استفاده شود؟ بله)0 خیر)1
+# آیا حروف کوچک انگلییسی داشته باشد؟ بله)0 خیر)1
+# آیا حروف کوچک انگلییسی داشته باشد؟ بله)0 خیر)1
+# آیا از حروف مخصوص مثل: /!@#$%^&*)( استفاده شود؟ بله)0 خیر)1
+
+# print('a^123')
