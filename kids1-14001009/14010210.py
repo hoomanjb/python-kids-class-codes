55046aea6a46ebfa55ff229bd545b2a428a29acc
hoomanjb <hoomanjavan74@gmail.com>
2022-04-30 17:35:45 +0330

    add kids14001009 codes

---
+++ kids1-14001009/14010210.py
@@ -0,0 +1,52 @@
+# print('hello world')
+
+a = [1, 2, 3]
+
+# user_input = input('ye add vared konid: ')
+
+# if user_input == '5':
+#     print('shoma adad panj ra vared kardid')
+# elif user_input == '6':
+#     print('shoma adad shesh ra vared kardid')
+#
+# if user_input == '5':
+#     print('shoma adad panj ra vared kardid')
+# if user_input == '6':
+#     print('shoma adad shesh ra vared kardid')
+
+a = [10, 12, 22, 1, 0, 23, 50]
+#
+# print(max(a))
+#
+# print(min(a))
+
+
+# hello = 'HELLO'
+# b = hello.lower()
+# print(hello)
+# print(b)
+#
+# c = 'asdasd'
+# print(c.upper())
+# print(c.count('h'))
+# print(c.endswith('o'))
+# print(c.find('l'))
+#
+# print(c.isalnum())
+# print(c.isdigit())
+# print(c.isnumeric())
+my_string = 'salam 12' # string
+my_integer = 34
+my_boolean = True
+my_list = [10, 12, 22, 1, 0, 23, 50, 10]
+
+print(my_list.count(10))
+my_list.sort(reverse=True)
+# print(my_list[::-1])
+print(my_list)
+print(my_list.pop())
+print(my_list.pop())
+print(my_list.pop())
+my_list.pop(2)
+print(my_list)
+
