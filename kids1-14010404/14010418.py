bf48fe1932b11809d72ed5419bbdc40031a6eca5
hoomanjb <hoomanjavan74@gmail.com>
2022-07-09 18:52:52 +0430

    add kids1-14010404-class

---
+++ kids1-14010404/14010418.py
@@ -0,0 +1,83 @@
+# first_name = 'hooman1'
+# last_name = 'hooman2'
+# user_phone_number = '654654'
+# user_age = 25
+
+# user first name
+# Camel Case -> userFirstName
+# Snake Case -> user_first_name
+# lower case -> userfirstname
+# Uper Case ->  USERFIRSTNAME
+# Title Case -> Userfirstname
+# Pascal Case -> UserFirstName
+
+# Special Char -> ! @ # $ % ^ & * ( ) - = + > <
+
+# اسم کاربر
+# first_name = 'hooman'
+
+# print(25)
+# print_ = 10
+
+# print(5 * 10)
+# print(5 + 10)
+# print(5 - 10)
+# print(5 / 10)
+
+# #################### Numeric ################ #
+# Integer -> adad tabiee  -> 0 ta 1000000
+# float -> adad ashariii ->  0.1, 0.9999
+# complex -> addad mokhtalet -> 3 + 4j
+
+
+# addad = 10
+# addad2 = 25
+
+# print(addad + addad2)
+# print(addad - addad2)
+# print(addad * addad2)
+# print(addad / addad2)
+#
+# sum_ = addad + addad2
+# minus = addad - addad2
+# prod = addad * addad2
+# divide = addad / addad2
+
+# print(type(sum_))
+# print(type(minus))
+# print(type(prod))
+# print(type(divide))
+# print(type('salam'))
+
+# oprator -> + - * / ** % //
+
+#print(2 ** 3)
+# 2 * 2 * 2 = 8
+
+#print(15 % 3)
+
+#print(100 * 0.2)
+# %20
+
+# print(22 / 6)
+# print(22 // 6)
+
+# olaviat -> () ** * / + -
+# a = 2 + 5 - 7 * 3 / 7 + 5
+# b = 2 + 5 * 3 / 7 + 5 - 7
+# c = 2 + 5 - 7 / 7 + 5 * 3
+
+# a = (2 + 5) - 9 * 3 / 7 + 9 # -> (2 + 5) , 7*3 , 21/7 , 21/7 + 9 , (2+5) - (21/7 + 9)
+# b = ((2 + 5) - 9) * (3 / (7 + 9))
+
+# print(a)
+# print(b)
+
+# a = 10
+# a = a + 5
+# a += 5  , a -= 5 , a /= 3
+# print(a)
+
+# print(pow(5045454, 7453453)) # 5 ** 7
+
+
