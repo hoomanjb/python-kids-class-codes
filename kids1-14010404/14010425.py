ced259e76309f16246cd834f1e20a4c984c5b2b1
hoomanjb <hoomanjavan74@gmail.com>
2022-07-16 18:37:07 +0430

    add 14010425 class

---
+++ kids1-14010404/14010425.py
@@ -0,0 +1,71 @@
+# ############################## #
+# nam:                           #
+# name khanevdagi:               #
+# age:                           #
+# email:                         #
+#                                #
+# b web site ma khosh amadid     #
+# ############################## #
+
+# nam = input('esme khodra vared konid: ')
+# name_khanevdagi = input('name khanevdagi khodra vared konid: ')
+# age = input('sene khodra vared konid: ')
+# email = input('email khodra vared konid: ')
+#
+# max_char = 100
+# fasele_max_ba_nam = max_char - len(nam) - 8
+# fasele_max_ba_name_khanevdagi = max_char - len(name_khanevdagi) - 20
+# fasele_max_ba_age = max_char - len(age) - 8
+# fasele_max_ba_email = max_char - len(email) - 10
+#
+#
+# print('# ' + '#' * 96 + ' #')
+# print(f'# nam: {nam}' + (' ' * fasele_max_ba_nam) + '#')
+# print(f'# name khanevdagi: {name_khanevdagi}' + (' ' * fasele_max_ba_name_khanevdagi) + '#')
+# print(f'# age: {age}' + (' ' * fasele_max_ba_age) + '#')
+# print(f'# email: {email}' + (' ' * fasele_max_ba_email) + '#')
+# print('# b web site ma khosh amadid' + (' ' * 71) + '#')
+# print('# ' + '#' * 96 + ' #')
+
+# ######################################################################### #
+import turtle
+
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+# t.forward(100)
+# t.home()
+# t.back(200)
+# t.right(60)
+# t.forward(100)
+# t.left(60)
+# t.forward(100)
+
+# t.forward(100)
+# t.right(10)
+# t.forward(10)
+# t.right(10)
+# t.forward(100)
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+turtle.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
