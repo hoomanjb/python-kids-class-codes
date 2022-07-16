b917b24123034f71dc3c4c50edde0d19c439d7ec
hooman <hoomanjavan74@gmail.com>
2022-07-16 14:54:17 +0430

    add class 14010420 - 21

---
+++ kids1-14010404/14010420.py
@@ -0,0 +1,110 @@
+# print(4 ** 4) # 4 * 4 * 4 * 4
+
+# import math
+# x = math.sqrt(9)
+# print(x)
+
+
+# #################### Boolean , 0,1 , Logic, Pc ############
+# x = True
+# y = False
+#
+# print(bool(0))
+# print(bool(1))
+# print(bool(321654))
+# print(bool('salam'))
+# print(bool(''))
+
+# print(5 > 6)
+# print(7 < 7)
+# print(3 <= 3)
+# print(3.5 > 8.1)
+# print(3 != 3)
+
+# name = 'hooman'
+# print('o' in name)
+# print('z' in name)
+# print('o' not in name)
+# print('z' not in name)
+
+# and , or
+# and -> True , False
+# 0 , 0 -> 0
+# 1 , 0 -> 0
+# 0 , 1 -> 0
+# 1 , 1 -> 1
+
+# or ->
+# 0 , 0 -> 0
+# 1 , 0 -> 1
+# 0 , 1 -> 1
+# 1 , 1 -> 1
+
+# print((3 < 7) and (2 > 6))
+# print((3 < 7) or (2 > 6))
+
+# print(not(3 < 7))
+# ################# STRING, Text, neveshtari ######
+
+# print("firs't'_name")
+# print('first_name')
+# print(type('salam'))
+
+# first_name = 'asdasdasdasdasdasd'
+# H O O M A N
+# 0 1 2 3 4 5
+# first_name = 'hooman'
+# print(first_name[3])
+# print(first_name[5])
+# print(first_name[-1])  # last
+# print(first_name[0])   # first
+# print(len(first_name))
+# var[start:end:step]
+# print(first_name[2:6:])    # -> 'namooh'
+# print(first_name[2::])
+# print(first_name[::-2])
+
+# text = 'hello ' + 'world' + '\n'
+# text += 10 * 'hello ' + '\n'
+# text += '.-+-.' * 20
+# print(text)
+
+# a = 'salam'
+# b = 'hello'
+# c = 'hi'
+
+# a, b, c = 'salam', 'hello', 'hi'
+# user_name = input('lotfan esme khodra vared konid: ')
+# age = input('senet ra vared kon: ')
+# education = input('tahsilat khod ra vared konid: ')
+#
+# text = f'{user_name} be web site ma khosh amadid.\n'
+# text += f'sene shoma {age} mibashad.\n'
+# text += f'tahsilate shoma {education}'
+#
+# text = f"""
+# {user_name} be web site ma khosh amadid.
+#     sene shoma {age} mibashad.
+#         madrake shoma {education}
+#
+# """
+#
+# print(20 * '*')
+# print(text)
+
+
+# case sensetive
+# a = 'py'
+# b = 'PY'
+#
+# print(a == b)
+
+# ################################
+# nam:                           #
+# name khanevdagi:               #
+# age:                           #
+# email:                         #
+# mr/ms:                         #
+#                                #
+# b web site ma khosh amadid     #
+##################################
---
+++ kids1-14010406/14010421.py
@@ -0,0 +1,17 @@
+# #################################
+first_name = 'hooman'
+age = 20
+phone_number = '13467986512'
+email_address = 'test@gmail.com'
+
+# ~ ! @ # $ % ^ * & ( ) - ? > < / \' "
+
+# print(first_name)
+# print(age)
+# print_ = 10
+# print('hello')
+a = 3.57
+
+print(type(first_name))
+print(type(age))
+print(type(3.57))
