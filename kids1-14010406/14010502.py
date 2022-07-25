70444e728d7b24bbd820e51520e926c0f0049728
hooman <hoomanjavan74@gmail.com>
2022-07-25 16:47:03 +0430

    add class 14010502

---
+++ kids1-14010406/14010502.py
@@ -0,0 +1,123 @@
+# STRING  (Text, neveshtari)
+
+# print('hello world')
+# print("hello world")
+
+# name = 'hooman'
+# print(type(name))
+#
+# a = 'Adsa!@#!@#433453nnns     111'
+# print(a)
+
+
+
+# name = 'nsdfsdfadfadlakdngkajdfngjkdnfgjksn'
+#       0123456789
+
+# print(name[2])
+# print(name[3])
+# print(name[0]) # first
+# print(name[-1]) # last
+# print(name[-10])
+# print(name[:5]) # name[start:end:step]
+# print(name[:9])
+# print(name[6:15])
+# name = '012345678910111213141516'
+# print(len(name))
+# print(name)
+# print(name[:len(name)+1])
+# print(name[:])
+# print(name[::])
+# print(name[:-1])
+#
+# print(name[7:-3])
+
+# print(name[::2])
+# print(name[::3])
+
+
+# name = 'anisa'
+# print('asina')
+# print(name[-6:-1])
+# print(name[-8:-1])
+# print(name[-1::])
+# print(name[::-1])
+# print(name[::-2])
+
+# name = ' hooman' + ' anisa'
+# print(10 * name)
+# print('*-*-*-' * 50)
+# print('hello' + ' world')
+
+
+# name = 'hooman'
+# print('z' in name)
+# print('h' in name)
+# print('H' in name)
+
+
+# print('hooman javan')
+# print('hooman')
+# print('javan')
+# print('{hooman\njavan\nanisa\t}}\b***\r***')
+
+# text = '''
+# asdasd
+# ada         sdasd
+# asdasda
+#             asdasda
+# sas             dasdasd
+# asdasda
+#                 sdasdasd
+# asdasdasd
+# '''
+# print(text)
+
+# name = input('name khodra vared konid: ')
+# age = input('sene khodra vared konid: ')
+# text = f"salam {name} sene shoma: {age}"
+# print(text)
+
+# a = '1'
+# b = '2'
+# c = '3'
+# a, b, c = '1', '2', 3
+
+
+
+
+# ############################## #
+# name:                           #
+# last_name:                     #
+# age:                           #
+# email:                         #
+#                                #
+#                                #
+# ############################## #
+
+# name = input('esme khodra vared konid: ')
+# last_name = input('name khanevadegi ro vared konid: ')
+# age = input('sene khodara vared konid: ')
+# email = input('email ra vared konid: ')
+#
+# max_box = 100
+#
+# print('# ' + (96 * '#') + ' #')
+# print('# ' + name + (34 * ' ') + ' #')
+# print('# ' + age + (38 * ' ') + ' #')
+# print('# ' + (96 * ' ') + ' #')
+# print('# ' + (96 * ' ') + ' #')
+# print('# ' + (96 * '#') + ' #')
+
+
+name = 'hooman'
+print('z' in name)
+print('z' not in name)
+
+print('a' == 'a')
+print('a' != 'a')
+
+email = 'echizi@gmail.com'
+print('gmail' in email)
+
+
