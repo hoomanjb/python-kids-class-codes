108954f4c71b0e9dc79175793b3a663f71539ba6
hooman <hoomanjavan74@gmail.com>
2022-07-26 16:57:37 +0430

    add class 14010503

---
+++ kids1-14010404/14010503.py
@@ -0,0 +1,112 @@
+# List
+
+# my_list = [2, 4, 8, 'hello', [6, 8, 7, [10, 11]]]
+a = 'hello'
+# print(type(my_list))
+# print(type(a))
+
+# print(a[3])
+# print(my_list[3])
+# print(my_list[-1])
+# print(len(my_list))
+my_list = [2, 4, 8, 'hello', [6, 8, 7, [10, 11, 14]]]
+
+# a = my_list[-1]
+# print(a[2])
+# b = my_list[-1]
+# c = b[-1]
+# print(c[-1])
+# print(my_list[-1][-1][1])
+# print(my_list[-1][-1])
+# print(my_list[-1][:3])
+# print(my_list[-2][1])
+# print(my_list[3][1])
+# a = 'salam'
+# # a[0] = 'S'
+# print(a)
+# a = a.split()
+# print(a)
+
+# print(my_list)
+# my_list[1] = 'salam'
+# print(my_list)
+# my_list.append('helloooo')
+# my_list.insert(2, 'goodby')
+# my_list.insert(2, [7, 9])
+# print(my_list)
+
+# a = 'hooman javan'
+# b = a.split()
+# print(a)
+# print(b)
+# print(b[0])
+
+# a = 'test@gmail.com'
+# b = a.split(sep='@')
+# c = a.split()
+# d = a.split(sep='t')
+# v = a[3:].split('t')
+# print(b)
+# print(c)
+# print(d)
+# print(v)
+# print(a.find('t', 1, 5))
+
+# a = 'salam'
+# print('s' in a)
+# print(a.find('z'))
+
+# ##############################
+# if elif else
+
+a = 5
+# if a < 10:
+#     print('salam chetori')
+#     print('ssssss')
+# elif a > 10:
+#     print(' a az 10 bozorgtar')
+# elif a > 20:
+#     print(' a az 20 bozorgtar')
+# else:
+#     print('a az 10 bozorgtare')
+
+# if a == 5:
+#     print(' a == 5 ast')
+
+# print('akhare dastan')
+
+
+# if (5 < 7 and 9 > 8):
+#     print('yechizi')
+
+# True 1 2 3 ' ' [1]
+# False 0 '' []
+
+# if 1:
+#     print('harchizi')
+#
+# if not 1:
+#     print('harchi')
+
+
+# user = input('name khodra vared konid: ')
+# age = input('sene khodara vared konid: ')
+# age = int(age)
+#
+# if age < 20:
+#     print('shoma ejaze vorod nadarid!')
+# else:
+#     print('khosh amadid!')
+
+
+# ye barname benevisid k esmo famil sen , shahr, address email
+# shahr bayad beyne 3ta shahr bood (tehran, karaj, rasht) -> 1, 2
+# addrese email chek konid hatman ba gmail.com -> 1, 2
+# agar 1 , 1 -> 1
+# agar 1 , 2 -> 2
+
+# ############################# #
+#
+#
+#
+# ############################# #
