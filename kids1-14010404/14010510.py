00a97a45428677a42321964611e2efadb8c83cc0
hoomanjb <hoomanjavan74@gmail.com>
2022-08-01 18:31:00 +0430

    add class 14010510

---
+++ kids1-14010404/14010510.py
@@ -0,0 +1,88 @@
+# 5ta soal harchi  5 + 10
+# agar karbar javabe dorost dad +1
+# emtiaze shoma 3 ast
+
+# q1 = '1- meghdar in moadele chand mishavad? (10+8=?) : '
+# q2 = '2- paytakhte iran kojast? : '
+# q3 = '3- avalin adade avale zoj? : '
+# q4 = '4- bozorgtarin heyvane khoshki? : '
+# q5 = '5- khsohbakht tarin adam haye rooye zamin da che keshvari hozor daran? : '
+#
+# a1 = input(q1)
+# a2 = input(q2)
+# a3 = input(q3)
+# a4 = input(q4)
+# a5 = input(q5)
+
+#
+# score = 0
+# if a1 == '18':
+#     score += 1
+#
+# if a2 == 'tehran':
+#     score += 1
+#
+# if a3 == '2':
+#     score += 1
+#
+# if a4 == 'fil':
+#     score += 1
+#
+# if a5 == 'iran':
+#     score += 1
+#
+# print(f'Soalate shoma tamam shode! emtiaze shoma: {score}')
+# #########################################
+# LOOP - (While (زمانی که تعداد تکرار مشخص نباشد و براساس شرایط حلقه شروع و متوقف میشود.), For (تعداد حلقه مشخص باشد))
+
+# counter = 0
+# while counter <= 1000:
+#     if not (counter % 2 == 0):
+#         print(counter)
+#     counter = counter + 1
+
+# score = 0
+# counter = 0
+# javabhaye_karbar = [a1, a2, a3, a4, a5]  # javab haye karbar
+# javabhaye_dorost = ['18', 'tehran', '2', 'fil', 'iran']  # javabhaye dorost
+#
+# while counter < 5:
+#     if javabhaye_karbar[counter] == javabhaye_dorost[counter]:
+#         score += 1
+#     counter += 1
+#
+# print(score)
+
+# counter = 0
+#
+# while True:
+#     if counter == 100:
+#         break
+#     if counter % 5 == 0:
+#         counter += 1
+#         continue
+#     print(counter)
+#     if counter % 2 == 0:
+#         print('adad zoj ast')
+#     else:
+#         print('adad fard ast')
+#     counter += 1
+#
+# print('harchi')
+
+# jadval zarb
+# 1, 10
+# print('1 2 3 4 5 6 7 8 9 10')
+# print('2 4 6 8 10 12 14 16 18 20')
+
+
+# [?, ?, ?, ?, ?]
+# apple
+#
+# 10 live
+#
+# print('[?, ?, ?, ?, ?]')
+# print('ye hads bezan (10 live)' )
+# print('[a, ?, ?, ?, ?]')
+# print('ye hads bezan')
+
