e08209d3642217b3d2d0e87ba9b52e891089975e
hoomanjb <hoomanjavan74@gmail.com>
2022-08-02 19:04:11 +0430

    add class code 14010511.py

---
+++ kids1-14010406/14010511.py
@@ -0,0 +1,84 @@
+# q1 = input('natije amaliate: 5 * 5 = ?: ')
+# q2 = input('paytakhte iran kojast? : ')
+# q3 = input('bozorgtarin keshvar jahan :')
+# q4 = input('bozorgtarin heyvane khoshki? : ')
+# q5 = input('shad tarin adamhaye rooye zamin bara che keshvari hastan? : ')
+#
+# a1, a2, a3, a4, a5 = '25', 'tehran', 'roosie', 'fil', 'iran'
+#
+# score = 0
+#
+# if q1 == a1:
+#     score += 1
+# if q2 == a2:
+#     score += 1
+# if q3 == a3:
+#     score += 1
+# if q4 == a4:
+#     score += 1
+# if q5 == a5:
+#     score += 1
+#
+# print(f'emtiaze shoma shode: {score}')
+
+# #######################################################
+# Loop , (while (زمانی استفاده میشه که ما تعداد حلقه رو ندونیم و براساس شرط یا شرایطی حلقه شروع و تمام میشود.) , for (زمانی استفاده میشه که تعداد لقه مشخص باشد.))
+
+# counter = 0
+# while counter <= 1000:
+#     if not counter % 2 == 0:
+#         print(counter)
+#     counter = counter + 1
+#
+# print('')
+
+# q1 = input('natije amaliate: 5 * 5 = ?: ')
+# q2 = input('paytakhte iran kojast? : ')
+# q3 = input('bozorgtarin keshvar jahan :')
+# q4 = input('bozorgtarin heyvane khoshki? : ')
+# q5 = input('shad tarin adamhaye rooye zamin bara che keshvari hastan? : ')
+#
+# a1, a2, a3, a4, a5 = '25', 'tehran', 'roosie', 'fil', 'iran'
+#
+# Q = [q1, q2, q3, q4, q5]
+# A = [a1, a2, a3, a4, a5]
+# score = 0
+# counter = 0
+# while counter < 5:
+#     if Q[counter] == A[counter]:
+#         score += 1
+#
+#     counter += 1
+# print(f'emtiaze shoma: {score}')
+
+# True 1 10 ' ' [1]
+# false 0 '' []
+
+# counter = 0
+# print('ghble halghe')
+# while True:
+#     counter += 1
+#     if counter == 20:
+#         break
+#     if counter % 2 == 0:
+#         continue
+#     print(counter)
+#
+# print('baade halghe')
+
+# ####################################
+# 10 Lives
+# apple -> [?, ?, ?, ?, ?]
+# ye harf hads bezan: a
+#  [a, ?, ?, ?, ?]
+# ye hads bezan:
+# [a, p, p, ?, ?] ! 1
+# [a, p, p, l, e] ! 2
+
+# Jadval zarb benevisin
+# 1 2 3 4 5 6 7 8 9 10
+# 2 4 6 8 10 12 14 16 18 20
+#
+
+
+
