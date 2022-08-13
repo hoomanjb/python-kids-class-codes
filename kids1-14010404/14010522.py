618713cb4b6364902651f23d6a1609b1abe611dd
hooman <hoomanjavan74@gmail.com>
2022-08-13 18:21:56 +0430

    add code class 1401052

---
+++ kids1-14010404/14010522.py
@@ -0,0 +1,64 @@
+# For
+# my_list = [1, 3, 6, 7, 9, 'hooman', [9, 10]]
+# for item in my_list[:3]:
+#     print(item)
+
+# my_string = 'Anisa'
+# for item in my_string:
+#     print(item)
+
+# my_list = [[2, 4], [1, 5], [8, 10]]
+# for item1, item2 in my_list:
+#     print(f'item1 = {item1} and item2 = {item2}')
+
+
+# for x in range(1, 11):
+#     print([x*y for y in range(1, 11)])
+#
+# for x in range(1, 11):
+#     result = []
+#     for y in range(1, 11):
+#         result.append(x*y)
+#     print(result)
+
+# ######################################### #
+# DRY = Don't Repeat Yourself
+
+# add
+# def add(a, b):
+#     c = a + b
+#     return c
+#
+# d = add(4, 6)
+# print(d)
+
+# import random
+#
+#
+# def give_random_words(my_list, count, duplicate):
+#     result = []
+#     for item in range(count):
+#         word = random.choice(my_list)
+#         if duplicate:
+#             result.append(word)
+#         elif word not in result:
+#             result.append(word)
+#         if len(result) == count:
+#             break
+#     return result
+#
+#
+# a = ['apple', 'orange', 'test', 'test2']
+# d = give_random_words(a, 2, False)
+#
+# print(d)
+
+# sang kaghaz gheychi
+# 5 dor - best 0f 5
+# sang > gheychi - gheychi > kaghaz - kaghaz > sang
+# user vs  pc
+# pc: 1  user 2
+# 5 dor ki barande shode
+
+
+
