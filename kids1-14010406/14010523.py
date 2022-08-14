d1df26b323810bd77afb104cb52642f2bc03ce21
hooman <hoomanjavan74@gmail.com>
2022-08-14 18:15:37 +0430

    add class 14010523

---
+++ kids1-14010406/14010523.py
@@ -0,0 +1,84 @@
+# While For
+# my_list = [1, 4, 6, 7, 'hello', 'test', [9, 0]]
+#
+# for item in my_list:
+#     print(item)
+
+# my_string = 'helooo1234'
+#
+# for item in my_string:
+#     print(item)
+
+# a = 10
+# print(type(a))
+# a = str(a)
+# print(type(a))
+
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# index_a = 1
+# while index_a <= 10:
+#     index_b = 1
+#     while index_b <= 10:
+#         result = index_a * index_b
+#         print(result)
+#         index_b += 1
+#     index_a += 1
+
+# for x in range(1, 11):
+#     result = []
+#     for y in range(1, 11):
+#         result.append(x*y)
+#     print(result)
+
+# for x in range(1, 11):
+#     result = [x * y for y in range(1, 11)]
+#     print(result)
+
+# for x in range(1, 11):
+#     print([x * y for y in range(1, 11)])
+#
+# for x in range(1, 11):print([x * y for y in range(1, 11)])
+
+# ########################################
+# DRY = Don't Repeat Yourself
+# define
+# 10% + 20000
+
+# def my_sum(kala, sood, hazine):
+#     c = (kala * sood) + hazine
+#     return c
+#
+# d = my_sum(1000, 0.1, 100)
+# f = my_sum(20000, 0.2, 700)
+# g = my_sum(88888, 0.1, 800)
+#
+# print(d)
+# print(f)
+# print(g)
+
+
+# def getting_user_input(message):
+#     user_input = input(message)
+#     return user_input
+#
+# a1 = getting_user_input('sadasd: ')
+# a2 = getting_user_input('aaasdasd: ')
+# a3 = getting_user_input('1111: ')
+#
+# print(a1)
+# print(a2)
+# print(a3)
+
+
+# sang kaghaz gheychi
+# pc , user
+# sang gheychi
+# 5dast
+# sang > gheychi  gheychi > kaghaz  kaghaz > sang
+
+
+
+
+
