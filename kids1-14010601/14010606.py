48f0e18b8b6ddefbe399be453548c73c359427a6
hooman <hoomanjavan74@gmail.com>
2022-08-29 17:01:28 +0430

    add class 140100606

---
+++ kids1-14010601/14010606.py
@@ -0,0 +1,70 @@
+# name = 'anisa'
+# my_sum = 5 + 87
+# print(name)
+# print(my_sum)
+
+# name = 'hooman'
+# Name = 'Hooman'
+#
+# first_name = 'hooman'
+# last_name = 'javanbakht'
+#
+# email_address = 'hoomanjavan@g.com'
+#
+#
+# na1me1 = 'anisa'
+
+# Special Char = ~!@#$%^&*()-?><|}{
+# name = 'anisa'
+
+# Reserved Words if for while
+# print(5 + 78)
+# print(5 + 78)  # inja ezafe konam
+# print(5 + 78)
+# print(5 + 78)
+# #################################################
+# Numeric  - Integer Float Complex
+a = 54
+b = -9
+c = 6.9
+d = -8.0
+e = 3j + 19
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# s = 9 + 10 # jaam
+# q = 4 - 1  # tafrigh
+# w = 4 * 6  # zarbe
+# r = 9 / 4  # taghsime
+# t = 7 % 3  # baghi mande
+# y = 3 ** 3 # tavan
+# u = 9 // 4 # taghsim va ronde b samte paiin
+# print(s)
+# print(q)
+# print(w)
+# print(r)
+# print(t)
+# print(y)
+# print(u)
+
+
+
+
+
+# ############################
+# String
+# 1,234,567,895
+# (a-z) (A-Z) (0-9) (special car)
+# 7+9
+# print(7+9)
+# print('7+9')
+# print('#!@$#$%#^%^&')
+
+
+phone = ''
+
+if phone == '':
+    print('shomare mobileto vared nakardi')
