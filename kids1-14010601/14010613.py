842e7ad07dcca3dd548238218402f4c7692a100c
hoomanjb <hoomanjavan74@gmail.com>
2022-09-04 18:57:12 +0430

    add code 14010613.py

---
+++ kids1-14010601/14010613.py
@@ -0,0 +1,136 @@
+# first_name = input('enter your first name: ')
+# last_name = input('enter your last name: ')
+# age = input('enter your age: ')
+# phone = input('enter your phone: ')
+#
+# max_char = 50
+# print(50 * '*')
+# print('* ' + 'first name: ' + first_name + ((34 - len(first_name)) * ' ') + ' *')
+# print('* ' + 'last name: ' + last_name + ((35 - len(last_name)) * ' ') + ' *')
+# print('* ' + 'age: ' + age + ((41 - len(age)) * ' ') + ' *')
+# print('* ' + 'phone: ' + phone + ((39 - len(phone)) * ' ') + ' *')
+# print(50 * '*')
+
+# ###############################################################
+# Frame work: Django - Flask - Fastapi
+# Game: Pygame - turtle
+# Data: Pandas - Numpy
+# Network , Security
+# Telegram
+
+import turtle
+
+s = turtle.getscreen()
+turtle.title('Anisa Class Room')
+turtle.bgcolor('yellow')
+t = turtle.Turtle()
+
+t.pensize(7)
+t.shapesize(2)
+t.speed(3)
+t.color('green')
+t.pencolor('red')
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.begin_fill()
+# t.color('yellow')
+# t.circle(100)
+# t.end_fill()
+
+# t.begin_fill()
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.end_fill()
+
+# t.goto(100, 100)
+# t.goto(-100, 100)
+# t.goto(-100, -100)
+# t.goto(100, -100)
+# t.goto(100, 100)
+
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.goto(0, 0)
+# t.goto(50, 50)
+# t.goto(0, 100)
+# t.goto(-50, 50)
+# t.home()
+# t.goto(50, 50)
+# t.goto(100, 0)
+# t.goto(50, -50)
+# t.home()
+# t.goto(100, 100)
+# t.goto(150, 50)
+# t.goto(100, 0)
+# t.home()
+# t.goto(0, 100)
+# t.goto(50, 150)
+# t.goto(100, 100)
+# t.home()
+
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.home()
+# t.goto(50, 50)
+# t.goto(0, 100)
+# t.goto(-50, 50)
+# t.home()
+# t.goto(50, -50)
+# t.goto(100, 0)
+# t.goto(0, 100)
+# t.goto(50, 150)
+# t.goto(150, 50)
+# t.goto(100, 0)
+# t.goto(50, 50)
+# t.goto(100, 100)
+
+
+# t.begin_fill()
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.home()
+# t.end_fill()
+t.penup()
+t.goto(100, 100)
+t.pendown()
+t.goto(-100, 100)
+t.goto(-100, -100)
+t.goto(100, -100)
+t.goto(100, 100)
+
+t.penup()
+t.goto(300, 0)
+t.pendown()
+t.circle(100)
+
+
+turtle.mainloop()
+
+
+
+
+
+
+
+
+
+
