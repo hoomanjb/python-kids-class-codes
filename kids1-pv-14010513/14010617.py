bba9fe497398a76f09f22dcb20d9798954559b3c
hooman <hoomanjavan74@gmail.com>
2022-09-11 16:57:43 +0430

    add class 140100617

---
+++ kids1-pv-14010513/14010617.py
@@ -0,0 +1,48 @@
+# print('hello world')
+
+# in moteghayer baraye esme khodete
+# name = 'hooman'  # test
+# my_name = 'amir hafez'
+#
+# print(name)
+# print(my_name)
+
+# ghavaede tarif kardane moteghayer
+# moteghayer ha mitoonan az horof koochik ya bozorg eng tarif beshavand
+# name = 1
+# Name = 1
+# NAME = 1
+# nokte: python b horof koochik o bozorg hasase - (case sensetive)
+# ye pishnehad: tooye python hamishe moteghayer haro horof koochik tarif mikonim
+
+# first_name = 'hooman'
+# last_name = 'javanbakht'
+# user_name = 123
+# phone_number = '3216548'
+#
+# name = 'hooman'
+# nemitavan aval kalameye moteghayer az addad estefade kard
+
+# special char -> ~!@#$$%^&*()+?><
+# first_name = 'hooman'
+
+# print(10)
+# print = 'hello'
+# print(10)
+
+# reserved words -> print while for  if not exec def del ......
+
+# ###########################################################
+# Numeric - integer float complex
+# int -> -10 -6 0 1 6
+a = 15
+print(type(a))
+
+b = 4.6
+print(type(b))
+
+c = 3j + 9
+print(type(c))
+
+
+
