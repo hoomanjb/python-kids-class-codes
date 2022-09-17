011164a6b1d90e4e634f9f2a98d4aa7acded6660
hoomanjb <hoomanjavan74@gmail.com>
2022-09-17 19:16:12 +0430

    add code 14010624.py

---
+++ kids1-pv-14010513/14010624.py
@@ -0,0 +1,144 @@
+# Numbers
+# + - * /
+# a = 4 + 8
+# print(a)
+
+# a = 16 / 3
+# b = 16 // 3
+# print(a)
+# print(b)
+
+# a = 5.99999999999999
+# b = int(a)
+# print(b)
+
+#  %  remain ya baghi mande
+# a = 17 % 3
+# print(a)
+
+# a = 4563455674 % 2
+# print(a)
+#
+# b = 41 % 2
+# print(b)
+
+# tavan
+# a = 3 ** 2
+# print(a)
+# b = 4 ** 2
+# print(b)
+
+# a = 5545321
+# print(a)
+# a = a + 345
+#
+# a += 345
+# a -= 100
+# a /= 2
+# a *= 2
+
+# ################################################
+# Boolean  -  logic ya manteghi
+# True - False
+# a = True
+# b = False
+# print(type(a))
+# print(type(b))
+
+# True -> 1 4 6 'a' "salam"  [2, 3]  {1, 2} {'name': 'hi'} (2, 3)
+# False -> 0  ''  ""  []  {}  ()
+
+# a = bool(0)
+# b = bool(1)
+# c = bool('')
+# d = bool('salam')
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# #####################################################
+# operator :  ==  !=  <  >   <=  >=   in    not in
+
+# print(5 == 5)
+# print(5 == 4)
+
+# a = 5
+# b = 7
+#
+# print(a > b)
+# print(a == b)
+# print(a < b)
+
+
+# a = 7
+# b = 7
+# print(a >= b)  # >=  <= !=
+
+# a = 5
+# b = 7
+# print(a == b)
+# print(a != b)
+
+
+# a = 4
+# print(a < 5 and a > 3)
+# print(a < 2 and a > 7)
+# print(a > 2 and a < 4)
+# print(a < 3 and a < 10)
+
+# and
+# 0 0 -> 0
+# 0 1 -> 0
+# 1 0 -> 0
+# 1 1 -> 1
+
+
+# or
+# 0 0 -> 0
+# 0 1 -> 1
+# 1 0 -> 1
+# 1 1 -> 1
+
+# a = 4
+# print(a < 5 or a > 3)
+# print(a < 2 or a > 7)
+# print(a > 2 or a < 4)
+# print(a < 3 or a < 10)
+
+
+# a = 5
+# print(not a < 10)
+# print(not a > 10)
+
+# ############################ # ############################ #
+# String - text
+
+# a = 'salam'
+# b = "salam"
+# c = '09124512123'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# a = 10
+# b = str(a)
+# print(b)
+# print(type(b))
+
+# a = 'a     salam        chetori            a'
+# print(a)
+# b = 'salam'
+# print(b)
+
+# a = 'salam '
+# b = 'chetori'
+# c = a + b
+# print(c)
+#
+# print('#' * 50)
+
+a = 'python str'
+#    0123456789
+
