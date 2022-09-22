b95b2a2bb03bffedf4b8a1af394b5dc0e7c3ef2d
hoomanjb <hoomanjavan74@gmail.com>
2022-09-22 08:42:56 +0330

    add code 7.py

---
+++ mg -14010505/7.py
@@ -0,0 +1,13 @@
+# print('hello')
+# a = input('plz enter your name: ')
+
+# define
+# def my_function(a, b):
+#     c = a + b
+#     return c
+#
+#
+# result = my_function(4, 9)
+# print(result)
+
+
