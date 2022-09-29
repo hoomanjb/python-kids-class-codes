249cca570a721e03de1268903baf99304d607be0
hoomanjb <hoomanjavan74@gmail.com>
2022-09-29 10:57:29 +0330

    add code 14010707.py

---
+++ kids1-pv-14010513/14010707.py
@@ -0,0 +1,74 @@
+# name = 'hoomansl;kjdfhn23rlksjdnf89dkjfnli2o09sdklfjs90odjf90g'
+#       012345
+# print(name[0])
+# print(name[1])
+# print(name[4])
+# print(name[5])
+# a = len(name)
+# print(len(name))
+# print(name[a - 1])
+# print(name[-1])  # last
+
+# name = 'salam khosh amadid aghaye {}'
+# a = name.format('amir hafez')
+# print(a)
+
+# ###########################################33
+# List
+# sequence (donbale ha) - order - changeable -
+# my_list = [2, 4, 6, 1.5, 6.9, 'hello', 'world', [6, 90, ['test', 'anisa']], 'salam']
+
+# print(my_list[0])
+# print(my_list[-1])
+# print(len(my_list))
+# print(my_list[5])
+# print(my_list[5][1])
+# print(my_list[5][-1])
+# print(my_list[7][1])
+# print(my_list[7][2][1])
+# print(my_list[7][2][1][-2])
+
+# print(my_list[1:4])
+# print(my_list)
+# my_list[1] = 'hhhhhhhh'
+# print(my_list)
+# my_list[:2] = [0, 0]
+# print(my_list)
+# a = [4, 5, 7, 10, 29]
+# print(type(a))
+# a.append(67)
+# print(a)
+# a.append([90, 78])
+# print(a)
+# a.insert(0, 99)
+# print(a)
+# a.insert(3, 99)
+# print(a)
+
+# #####################################
+# condition
+# a = 15
+# b = 7
+# if a < b:
+#     print('a az b koochiktare')
+# elif a == b:
+#     print('a ba b barabare')
+# elif a == 6:
+#     print('a barabare 6')
+# else:
+#     print('a az b bozorgtare')
+
+# print('harchi')
+
+# a = 6
+# b = 5
+#
+# if a != 6:
+#     print('ok')
+# if not b == 10:
+#     print('nok')
+
+
+
+
+
