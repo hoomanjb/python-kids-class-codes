3cc5a8f6c26e74625865e6ff782664547677e089
hoomanjb <hoomanjavan74@gmail.com>
2022-10-06 10:56:43 +0330

    add code 14010714.py

---
+++ kids1-pv-14010513/14010714.py
@@ -0,0 +1,122 @@
+import turtle
+
+turtle.title('Amir hafez')
+turtle.bgcolor('Salmon')
+s = turtle.getscreen()
+
+t = turtle.Turtle()
+t.speed(1)
+t.pencolor('DarkOrchid')
+t.pensize(5)
+
+# t.forward(100)
+# t.backward(200)
+# t.home()
+# t.right(45)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+
+# t.forward(100)
+# t.left(120)
+# t.forward(100)
+# t.left(120)
+# t.forward(100)
+
+# t.forward(100)
+# t.left(-60)
+# t.forward(100)
+# t.left(-60)
+# t.forward(100)
+
+# t.forward(100)
+# t.left(-60)
+# t.forward(100)
+
+# hexagon
+
+# t.forward(100)
+# t.left(60)
+# t.forward(100)
+# t.left(60)
+# t.forward(100)
+# t.left(60)
+# t.forward(100)
+# t.left(60)
+# t.forward(100)
+# t.left(60)
+# t.forward(100)
+
+# t.goto(100, 100)
+
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.goto(0, 0)
+#
+# t.goto(50, 50)
+# t.goto(0, 100)
+# t.goto(-50, 50)
+# t.home()
+#
+#
+# t.goto(50, 50)
+# t.goto(100, 0)
+# t.goto(50, -50)
+# t.home()
+#
+# t.goto(50, 50)
+# t.goto(100, 100)
+# t.goto(150, 50)
+# t.goto(100, 0)
+# t.home()
+#
+# t.goto(50, 50)
+# t.goto(100, 100)
+# t.goto(50, 150)
+# t.goto(0, 100)
+# t.home()
+
+# t.fillcolor('red')
+# t.begin_fill()
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.home()
+# t.end_fill()
+#
+# t.fillcolor('yellow')
+# t.begin_fill()
+# t.goto(100, 0)
+# t.goto(100, -100)
+# t.goto(0, -100)
+# t.home()
+# t.end_fill()
+#
+# t.fillcolor('green')
+# t.begin_fill()
+# t.goto(-100, 0)
+# t.goto(-100, -100)
+# t.goto(0, -100)
+# t.home()
+# t.end_fill()
+#
+#
+# t.fillcolor('blue')
+# t.begin_fill()
+# t.goto(-100, 0)
+# t.goto(-100, 100)
+# t.goto(0, 100)
+# t.home()
+# t.end_fill()
+
+t.circle(100)
+t.circle(-100)
+
+turtle.mainloop()
