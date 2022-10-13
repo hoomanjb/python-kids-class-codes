d23e82a84c2e092f688864ed911a996a2d4baf77
hoomanjb <hoomanjavan74@gmail.com>
2022-10-13 10:59:31 +0330

    add code 14010721.py

---
+++ kids1-pv-14010513/14010721.py
@@ -0,0 +1,28 @@
+# Loop: for , while
+
+# while -> zamani estefade mikonim k tedad halghe ro nadoonim
+# for -> zamani estefade mishe k tedad halghe ro bedoonim
+
+# a = 0
+
+# while a <= 100:
+#     if a % 2 == 0:  # odd - even
+#         print(a)
+#     if a % 2 != 0:  # odd - even
+#         print(a)
+#     a += 1
+# print('baade halghas')
+
+# a = 0
+# while True:
+#     print(a)
+#     if a == 9000000:
+#         break
+#     a += 1 # a = a + 1
+# print('ENDDDDDDDDDDDDDDdd')
+
+
+# 5 ta soal ro beporse
+# 3 bar ejaze bede talash kone
+
+
