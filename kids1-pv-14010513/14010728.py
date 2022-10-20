264b0f1fc1faf696e3af5affa01a62d0921f461b
hoomanjb <hoomanjavan74@gmail.com>
2022-10-20 11:03:38 +0330

    add file 14010728.py

---
+++ kids1-pv-14010513/14010728.py
@@ -0,0 +1,126 @@
+# 5 ta soal ro beporse
+# 3 bar ejaze bede talash kone
+
+# loop - (for, while)
+
+q1 = 'javabe soale : 2 + 2 ? '
+q2 = 'paytakht keshvare iran? '
+q3 = 'behtarin keshvar donya? '
+q4 = 'behtarin internet donya? '
+q5 = 'javabe soale : 2 * 2 ? '
+#
+score = 0
+a = 0
+# while a < 3:
+#     user_answer = input(q1)
+#     if user_answer == '4':
+#         print('CORRECT!!!')
+#         score += 1
+#         break
+#     elif a == 2:
+#         print('Eshtebah shode, dg nemitooni talash koni')
+#     else:
+#         print('Eshtebah shode, Dobare talash kon!')
+#     a += 1
+
+# ################################################################
+# for
+name = 'hooman'
+my_list = [1, 4, 6, 7, 3, 77]
+item = 10
+
+# for x in name:
+#     print(x)
+
+# for item in my_list:
+#     print(item)
+
+# for item in range(1, 11):
+#     print(item)
+
+# for item in range(3):
+#     user_answer = input(q1)
+#     if user_answer == '4':
+#         print('CORRECT!!!')
+#         score += 1
+#         break
+#     elif item == 2:
+#         print('Eshtebah shode, dg nemitooni talash koni')
+#     else:
+#         print('Eshtebah shode, Dobare talash kon!')
+
+# ########################################## #
+
+# def my_function(a, b):
+#     c = (a + b) + 1
+#     print(c)
+#
+#
+# d = my_function(4, 5)
+# print(d)
+
+# print('1')
+# print()
+# print('2')
+
+# def func():
+#     return 4 * 4
+#
+# a = func()
+# print(a)
+
+
+# DRY - Dont Repeat Yourself
+
+
+# q1 = '1-javabe soale : 2 + 2 ? '
+# q2 = '2-paytakht keshvare iran? '
+# q3 = '3-behtarin keshvar donya? '
+# q4 = '4-behtarin internet donya? '
+# q5 = '5-javabe soale : 2 * 2 ? '
+#
+# score = 0
+#
+# def validate(question, answer, score):
+#     for item in range(3):
+#         user_answer = input(question)
+#         if user_answer == answer:
+#             print('CORRECT!!!')
+#             score += 1
+#             return score
+#         elif item == 2:
+#             print('Eshtebah shode, dg nemitooni talash koni')
+#             return score
+#         else:
+#             print('Eshtebah shode, Dobare talash kon!')
+#
+# score = validate(q1, '4', score)
+# score = validate(q2, 'tehran', score)
+# score = validate(q3, 'iran', score)
+# score = validate(q4, 'iran', score)
+# score = validate(q5, '4', score)
+#
+# print(score)
+
+# def calculator(addad1, addad2, operator):
+#     if operator == '+':
+#         return addad1 + addad2
+#     elif operator == '-':
+#         return addad1 - addad2
+#     elif operator == '*':
+#         return addad1 * addad2
+#     elif operator == '/':
+#         return addad1 / addad2
+#     else:
+#         print('operator is not valid')
+#         return False
+#
+# print(calculator(5, 7, '*'))
+# print(calculator(5, 7, '-'))
+# print(calculator(5, 7, '+'))
+# print(calculator(5, 7, '/'))
+# print(calculator(5, 7, '$'))
+
+
+
+
