322bca73a13fcebaebd0a0ad264edcd9c4217a66
hoomanjb <hoomanjavan74@gmail.com>
2022-11-11 10:59:21 +0330

    add file 14010820.py

---
+++ ahk-pv-14010513/14010820.py
@@ -0,0 +1,50 @@
+# String
+
+# name = 'amirhafez'
+# print(type(name))
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.split(sep='@'))
+
+# select status from users where username = 'amirhafez' and password='123'
+# select status from users where username = '# drop users #' and password='123'
+# ORM - object relation mapping
+# User: unique - specialchar - 4> -
+# password: 1mah -
+# mobile - email -
+# amihafez@#$%
+
+# def amirhafez_spliter(name):
+#     r_index = name.find('r')
+#     return name[:r_index + 1] + ' ' + name[r_index + 1:]
+#
+# a = amirhafez_spliter('amirhafez')
+# print(a)
+
+# name = 'amir hafez'
+# print(name[:5 + 1])
+
+# name = 'amirhafez'
+# print(name.startswith('z'))
+# print(name.replace('a', 'A', 1))
+# print(name[:1].replace('a', 'A'))
+# print(name.isdigit())
+# print(name.isprintable())
+# name = '      amir       '
+# print(name.isspace())
+# print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+# print(name)
+# name_striped = name.strip()
+# print(name)
+name = 'amirhafez'
+print(name.count('g'))
+print(name.index('r'))
+print(name.index('g'))
+# print(name.find())
+
+
+
+print('harchiiiiiiiiiiiiiiiiiiii')
