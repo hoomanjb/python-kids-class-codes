0229344cf93a28039fa9a281b720d824f5ce310e
hoomanjb <hoomanjavan74@gmail.com>
2022-11-14 17:50:05 +0330

    add file 8.py

---
+++ mg -14010505/8.py
@@ -0,0 +1,115 @@
+# data type:
+# string
+name = 'MAGGI'
+phone_number = '12356654321'
+
+# Numbers
+a = 10  # integer
+b = 4.6  # float
+c = 3 + 7j  # complex
+
+# boolean
+x = True
+y = False
+
+# list -> ordered
+# my_list = [2, 3, 5, 6, 'hello', 'hooman', 5, 6, [4, 7, 8, 8, [55, 66, 77], 9, 10], 'byebye']
+
+# print(my_list[1])
+# print(my_list[0])
+# print(my_list[4])
+# print(my_list[9])
+# print(my_list[-1])
+# print(my_list[-2])
+# print(my_list[8][3])
+# print(my_list[8][4][1])
+# print(my_list[8][5])
+# print(my_list[-1][1])
+
+# name = 'Maggi'
+#
+# print(name[2])
+
+# a = 10
+# b = 5
+
+# if a < b or a > 50:
+#     print('a is qt b')
+# else:
+#     print('a is not gt b')
+
+
+# a = 10
+# b = 5
+# if a > 5 and a < b:
+#     print('ok')
+# else:
+#     print('Nope')
+
+# OR : 0=False  1=True
+# False or False -> False
+# False or True  -> True
+# True or False  -> True
+# True  or True  -> True
+
+# AND
+# False and False  -> False
+# False and True  -> False
+# True and False  -> False
+# True and True  -> True
+
+# a = 10
+# b = 7
+# c = 2
+#
+# if a > 50:
+#     if c > b:
+#         print('1')
+#     else:
+#         print('2')
+# elif a > b:
+#     if b > 5:
+#         print('3')
+#     else:
+#         print('4')
+
+#
+# Loop , (while , for)
+# For
+# while
+
+# while True:
+#     user_input = input('enter your gmail address: ')
+#     if '@gmail.com' in user_input:
+#         print('thanks!!!')
+#         break
+#     else:
+#         print('you must enter your gmail address!!!')
+#
+# name = 'Maggi'
+# for item in name:
+#     print(item)
+
+# my_list = [4, 6, 7, 1, 2, 'hi', [8, 6]]
+#
+# for item in my_list:
+#     print(item)
+
+
+# for item in range(10000000):
+#     print(item)
+
+
+# ###############################
+# function
+
+def my_sum(a, b):
+    c = a + b
+    return c
+
+result = my_sum(3, 7)
+print(result)
+
+
+
+
