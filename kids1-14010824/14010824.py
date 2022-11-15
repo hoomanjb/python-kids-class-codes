f2610a4a3199d01c7c6e93c5304879b1b73ea9e5
hoomanjb <hoomanjavan74@gmail.com>
2022-11-15 19:59:32 +0330

    add file 14010824.py

---
+++ kids1-14010824/14010824.py
@@ -0,0 +1,90 @@
+# print('hello world')
+# print('سلام خوبی')  # full unicode
+# print(3 + 6)
+
+# variable = متغیر
+# value = مقدار
+# a = 10
+# print(a)
+#
+# name = 'helma'
+# print(name)
+# a = 10  # in moteghayer baraye nesbate felan darsad estefade mishe
+
+# syntax = مدل نگارش = قوانین پایتون میگه = راعتش کنی اجباری
+# b = 9
+
+# ######################################### #
+# متغیر ها باید با حروف کوچیک اینگلیسی تعریف شوند.
+# حتما سعی شود اسم متغیر متناسب با مقدار باشد.
+# phone_number = 'hooman'
+# email_address = 'test@gmail.com'
+# اسم متغیر حتما معنا دار باشد.
+# a = 10
+# اگر اسم متغیر چند کلمه ای یا چند بخشی باشه از _ استفاده میکنیم.
+# emailaddres = 'eeet'
+# NAME = 'anisa'
+# در تعرریف اسم متغیرها نباید از کارکترهای مخصوص @$%^&*)(!×÷ کنیم
+# name = 'anisa'
+# پایتون به حروف بزرگ و کوچیک حساسه case sensetive
+# a = 10
+# A = 10
+# nAme = ' anisa'
+# naMe = 'anisa'
+# از کلمات رزرو پایتون نباید استفاده کرد
+# print = 20
+
+# a = 10
+# a = 20
+# a = 30
+
+# ############################################# #
+# a = 10
+# a = 'salam'
+# a = True
+# a = [2, 3]
+
+# Integer a = 10
+# a = 7
+# if a < 5: {
+#  print(123)
+# } curve bracket
+# ; #  - [] bracket - square bracket
+# print(1)
+
+# Numbers
+# integer = اعداد صحیح = int
+# ..., -2, -1, 0, 1, 2, 3 ...
+# a = 6
+# print(type(a))
+
+# float = اعداد اعشاری
+# b = 4.6
+# print(type(b))
+
+# complex = اعداد مختلط
+# d = 5 + 7j
+# print(type(d))
+
+# a = 7
+# b = 9
+#
+# d = a + b  # جمع
+# print(d)
+# e = a - b  # تفریق
+# print(e)
+# f = a * b  # ضرب
+# print(f)
+# g = a / b  # تقسیم
+# print(g)
+# h = a // b  # تقسیم (رند شده)
+# print(h)
+# k = a % b  # باقیمانده تقسیم
+# print(k)
+
+# print(3645172183746 / 23423)
+# print(2340598237450 * 999)
+# print(823847298347 ** 12387)
+
+import math
+print(math.pi)
