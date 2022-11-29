e3a4a2bb61784115829f8755d2b411527936a1b4
hoomanjb <hoomanjavan74@gmail.com>
2022-11-29 16:25:34 +0330

    add file 14010908.py

---
+++ kids1-14010824/14010908.py
@@ -0,0 +1,73 @@
+text = 'Hello World'
+
+# print(text[1])
+# print(text[0])
+#
+# print(len(text))
+# print(text[1:5])
+# print(text[:7:2])
+
+# text = 'Hello World'  # dlroW olleH
+# print(text[::1])
+# print(text[::-1])
+
+# result = 'Hello' + ' ' + 'world'  # 'Hello world'
+# print(result)
+#
+# result = '#' * 50
+# print(result)
+
+
+# a = 10
+# b = 'hello'
+# c = '12364354852'
+#
+# a, b, c = 10, 'hello', '29'
+#
+# a = 'h'
+# b = 'H'
+# print(a == b)
+
+# text = 'test@gmail.com'
+# national_code = '6542135421'
+# phone_number = '09321365321'
+# print('@gmail.com' in text)
+
+
+# #######################################
+# text = 'hello world'
+# Format String
+# import datetime
+#
+# name = input('enter your name: ')
+# age = input('enter your age: ')
+#
+# current_date = datetime.datetime.now()
+#
+# text = f'Welcome to our web site {name} your age is {age} and time is {current_date}'
+# print(text)
+
+
+# ############################################################
+# List
+
+# []  bracket , squre bracket
+# ()
+# {}  cure bracket
+
+# my_list = [3, 5, 6, 'Hello', 'world', 99, [7, 'a'], 55, 'bb']
+# print(my_list)
+
+# ###################################### #
+# Project
+# first_name , last_name , age , kode_meli
+# ye matn khosh amad gooii
+
+# ###################################### #
+
+
+
+
+
+
+
