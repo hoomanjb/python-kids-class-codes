d32a737f6d40dab999d46bbfb7ef749be37025cc
hoomanjb <hoomanjavan74@gmail.com>
2022-12-13 19:49:25 +0330

    add file 14010922.py

---
+++ kids1-14010824/14010922.py
@@ -0,0 +1,143 @@
+# Cloud -> 1
+# List
+
+
+# my_list = [1, 3, 4, 5, 'hello', '9', 0, True, False, [9, 12, 10, ['salam'], 445, 45], 'world']
+
+# print(my_list)
+# print(type(my_list))
+# print(my_list[0])
+# print(my_list[6])
+# print(my_list[4])
+# print(my_list[9])
+# print(my_list[9][0])
+# print(my_list[9][3][0])
+# print(len(my_list))
+# print(my_list[4])
+# print(my_list[4][1])
+
+# print(my_list[1:5])
+# my_list = [1, 3, 4, 5, 'hello', '9', 0, True, False, [9, 12, 10, ['salam'], 445, 45], 'world']
+#
+# print('hello' in my_list)
+#
+# print('salam' in my_list[9][3])
+
+
+# #######################################################################
+# if - else --- loop
+
+# if
+
+
+# a = 15
+# if a > 10 and a < 20:
+#     print('a az 5 bozorgtar ast')
+#     print('1')
+#     print('2')
+#
+# print('tamaaam')
+
+# False = 0  , True = 1
+# AND
+# 0 and 0 -> False
+# 1 and 0 -> False
+# 0 and 1 -> False
+# 1 and 1 -> True
+
+# a = 5
+# b = False
+# if a > 10:
+#     print('a az 5 bozorgtar ast')
+#     print('1')
+#     print('2')
+#     b = True
+#
+# print('tamaaam')
+# print(b)
+
+# False = 0  , True = 1
+# or
+# 0 or 0 -> False
+# 1 or 0 -> True
+# 0 or 1 -> True
+# 1 or 1 -> True
+
+# a = 100
+# b = 100
+#
+# if a > b:
+#     print('a az b bozorg tar ast')
+# elif b > a:
+#     print('a az b koochik tar ast!')
+# elif a == 10:
+#     print('a == 10 ast')
+# else:
+#     print('harchi!!!!')
+#     print('harchi!!!!11')
+#     print('harchi!!!!222')
+#     print('harchi!!!!33')
+#     print('tamaaaam')
+
+# True 1 ' ' [1]
+# False 0 '' []
+
+# phone_number = ''
+# if not phone_number:
+#     print('ok')
+
+# a = 7
+# b = 10
+#
+# if a < b and a == 10 or b > 2 and a < 3 and b == 10:
+#     print('ok')
+#     print(1)
+
+# ##########################################
+# match - case
+
+# a = 'salam'
+
+# match a:
+#     case 'hello':
+#         print('a is hello')
+#     case 'hi':
+#         print('a is hi')
+#     case 'yes':
+#         print('a is yes')
+#     case _:
+#         print('a is somthing else')
+#
+# if a == 'hello':
+#     print('a is hello')
+# elif a == 'hi':
+#     print('a is hi')
+# elif a == 'yes':
+#     print('a is yes')
+# else:
+#     print('a is somthing else')
+
+
+# ########################################################
+# loop (while, for)
+
+# while -> zamani k nadoonim che tedad halghe gharare ijad konim
+# for -> zamani k midoonim che tedad halghe gharare ijad beshe
+
+
+# while
+a = 1
+
+while a < 100:
+    if a % 2 == 0:
+        if a in [10, 20, 30, 40, 50, 60, 70, 80, 90]:
+            print('ssssssssss')
+        print(a)
+    a += 1
+
+
+
+
+
+
+
