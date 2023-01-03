d8330113ef353d2ff4b0ac180df1dbdeec41dd7a
hoomanjb <hoomanjavan74@gmail.com>
2023-01-03 19:53:55 +0330

    add file 1401013.py

---
+++ kids1-14010824/14011013.py
@@ -0,0 +1,70 @@
+# loop (while, for)
+
+# while -> zamani k nadoonim che tedad halghe gharare ijad konim
+# for -> zamani k midoonim che tedad halghe gharare ijad beshe
+
+# a = 1
+#
+# while a < 100:
+#     if a % 2 == 0:
+#         if a in [10, 20, 30, 40, 50, 60, 70, 80, 90]:
+#             print('ssssssssss')
+#         print(a)
+#     a += 1
+
+# a = 5
+# while a > -5:
+#     print(a)
+#     a -= 1  # a = a - 1
+#
+# print('END')
+
+# a = 0
+#
+# while a < 1000:
+#     print(a)
+#     a += 1
+
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+# print('END')
+
+
+# a = 0
+# while a <= 1000:
+#     if a % 2 == 0:
+#         print(a)
+#     if a == 850:
+#         # break  # baraye shekastane halghe
+#         a += 10
+#         continue  #
+#     a += 1
+
+# ########################################
+# for
+# my_list = [2, 3, 5, 6]
+#
+# for item in my_list:
+#     print(item)
+
+my_text = 'hello world'
+
+for item in my_text:
+    print(item)
+
+a = 0
+while True:
+    print('hello')
+    a += 1
+    if a >= 10:
+        break
+
