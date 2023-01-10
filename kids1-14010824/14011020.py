660ef5583fc85931e0a42f902d0be15ba5673317
hoomanjb <hoomanjavan74@gmail.com>
2023-01-10 19:53:51 +0330

    add file 14011020.py

---
+++ kids1-14010824/14011020.py
@@ -0,0 +1,136 @@
+# GUI
+# library ,
+# RGB
+
+# import turtle
+#
+# turtle.getscreen()
+# t = turtle.Turtle()
+# t.speed(1)
+
+# t.forward(100)
+# t.right(90)
+# t.right(45)
+# t.left(45)
+# t.forward(100)
+
+# t.circle(100)
+
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+#
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+#
+# t.circle(100)
+
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+
+# t.goto(100, 0)
+# t.goto(100, -100)
+# t.goto(0, -100)
+# t.goto(0, 0)
+
+# t.goto(100, 0)
+# t.goto(150, -50)
+# t.goto(100, -100)
+# t.goto(0, -100)
+# t.goto(-50, -50)
+# t.home()
+
+# t.goto(100, 0)
+# t.goto(50, 100)
+# t.home()
+
+# turtle.bgcolor('green')
+# turtle.title('ANISA CLASS')
+# t.pensize(5)
+# t.shapesize(3,3,3)
+# t.pencolor('blue')
+#
+# t.fillcolor('red')
+# t.begin_fill()
+#
+# t.goto(100, 0)
+# t.goto(50, 100)
+# t.home()
+#
+# t.end_fill()
+#
+# t.fillcolor('blue')
+# t.pencolor('yellow')
+# t.begin_fill()
+# t.speed(3)
+# t.circle(50)
+#
+# t.end_fill()
+
+
+# t.fillcolor('blue')
+# t.pencolor('yellow')
+# t.begin_fill()
+# t.speed(3)
+# t.circle(100)
+# t.end_fill()
+#
+# t.fillcolor('pink')
+# t.pencolor('green')
+# t.begin_fill()
+# t.speed(2)
+#
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.home()
+#
+# t.end_fill()
+#
+# turtle.mainloop()
+
+# ######################################################
+# یه برنامه ای بنویسید
+# 10 تا سوال از کاربر بپرسه
+# 2+2=؟
+# 2 بار میتونه تلاش کنه جواب درست بده
+# 5
+# 6
+# 4
+# یه امتیاز بهش میدین
+# از 10 تا سوال 5 تارو جواب دادی پس امتیازت میشه 5
+
+# اگه دفعه اول جواب درست داد 3
+# 2
+# 1
+# 0
+
+# natijeye 2 + 2 = ? chand mishe: A)4 B)3 C)6 D)2
+# A - 4
+
+# ##################################################
+
+
+# function
+# تابع
+# یک عملی قراره چندین بار تکرار بشه
+
+
