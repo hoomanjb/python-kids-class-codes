2887ad29a2b910e78184ae04ef4353bdf8a1b2b5
hooman <hoomanjavan74@gmail.com>
2023-02-14 20:02:16 +0330

    add 14011125.py

---
+++ kids1-14011111/14011125.py
@@ -0,0 +1,83 @@
+# print(52+36)
+# name = 'anisa'  # string کارکتر نوشتنی
+# x = 10  # عدد صحیح  integer
+# y = 15
+# z = x * y + 10
+# print(z)
+# print(name)
+#
+# x = 20
+# x = 'salam'
+# x = 2
+# x = []
+# print(x)
+
+# ########## #
+
+# من دارم اینجا تست میکنم
+# print(1)
+# print(2)  # injaaaa
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+
+# syntax قانون نگارش پایتون
+# pep قاعده استاندارد
+
+# ############################
+# variable متغیر
+
+# اسم متغیر باید معنادار و متناسب با مقدارش باشد
+# variable = value
+# مقدار = متغیر
+
+name = 2183618721
+a = 20
+email = 'test@gmail.com'
+name = 'هومن'
+
+# حتما باید از کارکترهای حروف لاتین و کوچک استفاده شود
+address = 'yechizi'
+
+# اگه چند کلمه ای یا چندبخشی بود
+# underline , underscore , خط تیره
+phone_number = 1231232
+first_name = 'asda'
+
+
+
+name = 'ali'
+name2 = 'reza'
+n2ame = 'mehdi'
+
+# special Char: ~!@#$%^&*()-+?
+# $name = 'reza'
+
+phone_number = 123123
+
+# snake case  -> first_name
+# camel case ->
+# pascal case ->
+# upper case -> FIRSTNAME
+# lower case -> firstname
+
+# reserve words
+# print(5 + 9)
+# print = 10
+#
+# b = 5 + print
+
+
+a = 10
+b = 'hello'  # سینگل کوتیشن - تک کوتیشن
+c = "hello"  # دابل کوتین یا دوبل کوتیشن
+
+d = "hello 'world"
+print(d)
+e = 'hello "world'
+print(e)
+
+# کد تمیز - کد کثیف - کد نابود - قشنگی کد
