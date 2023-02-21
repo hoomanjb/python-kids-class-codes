595cec29eecda2e07a71ec7a59e7dd3d1f6a9992
hooman <hoomanjavan74@gmail.com>
2023-02-21 20:08:08 +0330

    add 14011202.py

---
+++ kids1-14011111/14011202.py
@@ -0,0 +1,207 @@
+# DataType = نوع داده
+# Datas اطلاعات داده هاس
+# data =
+# Numbers (Integers, Float, Complex)
+# Boolean - bool (0,1 logic منطق) True-False
+# Dict dictionary دیکشنری
+# Set مجموعه
+# sequence دنباله (List, Tuple, String)
+
+# ###################################
+# Numbers (Integers, Float, Complex)
+# integer عدد صحیح
+# a = -1
+# b = 4
+# c = 0
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# float اعشاری
+# e = 5.1
+# f = -0.3
+# g = 0.0
+# print(type(e))
+# print(type(f))
+# print(type(g))
+
+# complex  اعداد مختلط
+# t = 3j+9
+# print(type(t))
+
+# a = 5
+# b = float(a)
+# print(b)
+# c = int(b)
+# print(c)
+
+# a = 5
+# b = 9
+# d = a + b  # جمع
+# print(d)
+# c = a * b  # ضرب
+# print(c)
+# f = a - b  # تفریق
+# print(f)
+# g = a / b  # تقسیم
+# print(g)
+# h = a // b  # تقسیم با رند کردن برای تبدیل شده به عدد صحیح
+# print(h)
+# j = a ** b  # توان
+# print(j)
+
+# import math
+#
+# a = 25
+# print(math.sqrt(a))
+
+# a = 11
+# b = 5
+# c = a / b
+# print(c)
+# d = a % b
+# print(d)
+
+# a = 18771246928734692873428734627
+# b = 3
+# c = a % b
+# print(c)
+
+# #############################################
+# Boolean
+# True
+# False
+# a = True
+# b = False
+# c = 'True'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# True 2 22 's' ' ' [5]
+# False 0 '' []
+
+# a = 0
+# print(bool(a))
+# b = ''
+# print(bool(b))
+# d = ' '
+# print(bool(d))
+# e = 5
+# print(bool(e))
+
+
+# a = 5
+# b = a + 1
+# print(a)
+# a = a + 1
+# a += 1  # a = a + 1
+# a -= 1  # a = a - 1
+# a *= 2  # a = a * 2
+# print(a)
+
+
+# #######################################
+# a = 5
+# b = 7
+# print(a < b)  # کوچکتر بودن
+# print(a > b)  # بزرگتر بودن
+# print(a <= b)  # کوچکتر مساوی
+# print(a >= b)  # بزرگتر مساوی
+# print(a == b)  # برابری
+# print(a != b)  # نابرابری
+
+# AND  OR
+
+# AND  0=False  1=True
+# 0 and 0 = 0
+# 0 and 1 = 0
+# 1 and 0 = 0
+# 1 and 1 = 1
+
+# a = 17
+# b = 5
+# print(a > b and a > 10)
+
+# OR  0=False  1=True
+# 0 or 0 = 0
+# 0 or 1 = 1
+# 1 or 0 = 1
+# 1 or 1 = 1
+
+# a = 7
+# b = 5
+# print(a > b or a > 10)
+
+
+# a = 7
+# b = 5
+# c = a > b
+# print(not c)
+
+# ##################################
+# String
+# name = 'hello world'
+# name2 = "hello world"
+# print(type(name))
+# adad = '150'
+# adad2 = 150
+# a = int(name)
+# print(a)
+
+# a = '232'
+# b = int(a)
+# print(b)
+
+
+# name = 'Hello worldelakjbgldkjfgblsdkjgblskjdfgblsjdkfgblksdjfgn;sfjkgnsldjkfgsndkfjgnskjjbg'
+#       01234567891011
+# print(name[0])
+# print(name[7])
+# print(name[5])
+# print(name[10])
+# print(name[15])
+# print(len(name) - 1)
+# print(name[-1])
+# print(name[83])
+# print(name[-2])
+#
+# print(len(name))
+# print(name[0])
+# print(name[-84])
+#
+# print(len(name) - 1)
+# print(name[-1])
+
+# name = 'Hello worldelakjbgldkjfgblsdkjgblskjdfgblsjdkfgblksdjfgn;sfjkgnsldjkfgsndkfjgnskjjbg'
+#       012345
+# print(name[5:60])
+# print(name[1:5])
+# print(name[0:11])
+# print(name[:11])  # name[start:end]
+# print(name[50:])
+
+# [start:end:step]
+# print(name[:60:3])
+# print(name[:60])
+# print(name[:60:1])
+
+# print(name[7:-4])
+
+name = 'anisa'
+# print(name)
+# print(name[:])
+# print(name[0:])
+# # asina
+# print(name[::-1])
+
+# a = 'hello'
+# b = ' world'
+# c = a + b
+# print(c)
+
+
+# a = 'hello '
+# print(a * 100)
+
+
