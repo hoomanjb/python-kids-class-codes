dc52b6287c71326caa8a8933f47b34b6d433f7d6
hooman <hoomanjavan74@gmail.com>
2023-04-04 20:51:36 +0430

    add 14020115.py

---
+++ kids1-14011111/14020115.py
@@ -0,0 +1,102 @@
+# loop - while, for
+# while -> تا وقتی که - وقتایی که تعداد حلقه مشخص نیست
+# for -> برای - وقتایی که تعداد حلقه مشخص هستش
+
+# a = 1
+# b = 10
+# while a <= 3 or b > 10:
+#     print('salam')
+#     print(a)
+#     print('####')
+#     a += 1
+#
+# print('Injaaaaaaaaa')
+
+
+# a = 0
+# while True:
+#     if a % 2 != 0:
+#         a += 1
+#         continue  # برای وقتایی که حلقه رو بخایم جلو بندازیم
+#     print(a)
+#     a += 1
+
+# True  2 4 55 'a' [5]
+# False 0 '' []
+a = 0
+# while True:
+#     print(a)
+#     a += 1
+#     if a == 50:
+#         print('injaaaaaa')
+#         break
+#     print('###')
+
+
+# hasan
+# 1234 -> ;ldkgj;gklj;2l3k4j;1lk23n4;1lknbjhbjhv2hg3cvkjdfdl
+# Database -> uuid4 -
+
+# hasan
+# 567 -> 1h2kj13lkj12h3nlk1j23hlk1j2
+
+# 123 - ok
+# 567
+# 567 - ok
+# ########################################
+# for
+# my_list = [6, 7, 3, 2]
+# for item in my_list:
+#     print(item)
+# a = range(1, 1000)
+# print(a[30])
+
+# for item in range(1, 1000):
+#     print(item)
+
+# user: ali#
+# pass:
+
+# text = 'ali#'
+# for item in text:
+#     if item == '#':
+#         print('ERROR')
+#
+# if '#' in text:
+#     print('ERROR')
+
+# for item in range(100_000):
+#     if item % 2 == 0:  # زوج
+#         print(item)
+
+# جدول ضرب
+# 1*1 1*2 1*3  1*10
+#
+# 10*1 10*2    10*10
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         result = i * j
+#         # print(f'{i} * {j} = {result}')
+#         print(result)
+#     print('#########')
+
+
+# for i in range(1, 11):
+#     result = ''
+#     for j in range(1, 11):
+#         result += f'{i*j:4}'  # یعنی یک بلوک کارکتر 4تایی بساز و پرش کن
+#     print(result)
+
+# ##############################
+# tamrin
+# esme karbar ro beporsin
+# 10 ta soal az karbar bepporse
+# socre +1
+# 3bar talash kone
+# 7ta soal javab dadi emtiazet mishe 7
+# mikhay edame bedi ya na?
+# are: esmesho beporsin
+# na: tamoom
+
+
+
