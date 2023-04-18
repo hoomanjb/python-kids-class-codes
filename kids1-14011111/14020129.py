cf0117c2aee13a67447ae9e17e2976e483012c4c
H.Javanbakht <h.javanbakht@emofid.com>
2023-04-18 19:48:34 +0330

    add 14020119.py

---
+++ kids1-14011111/14020129.py
@@ -0,0 +1,121 @@
+# Function
+# تابع
+
+# x = a**2 + 3*b - 10
+# define تعریف کردن
+
+# def my_function(a, b):
+#     f = a ** 2
+#     g = 3 * b
+#     x = f + g - 10
+
+# اسم متغیر باید حتما باید از قوانین اسم گذاری متغیرها پیروی کند.
+# def my_func1():
+#     print('salam chetori?')
+#     return 10
+#
+#
+# b = my_func1()
+# print(b)
+# my_func1
+
+# a = print('hello')  # خروجیه تابع پرینت رو داخل متغیر ذخیره کن
+# b = print  # یعنی خوده تابع رو بریز داخلش
+# print(a)
+# print(b)
+
+
+# a = input('enter your name: ')
+
+
+# def my_function(a, b):
+#     f = a ** 2  # a * a - a * a * a
+#     g = 3 * b
+#     x = f + g - 10  # 16 + 15 - 10
+#     return x
+
+# print(my_function(4, 5))
+# print(my_function(5, 7))
+# print(my_function(2, 1))
+# print(my_function(8, 8))
+# print(my_function(3, 99))
+# print(my_function(345345345, 234234234))
+# print(my_function(234234, 2342342))
+
+# print(my_function('salam', 5))  # wrong
+
+
+# ############################################# #
+# backend - backend developer
+# front - front developer
+# design
+
+# احراز هویت
+# check kardane kode meli
+# 233244523453
+# 132163546132
+# 321654874351
+
+# #############################
+
+# def check_input(name):
+#     if len(name) > 10:
+#         print('Wrong input!')
+#     elif name in ['#', '$', '!']:
+#         print('Wrong Input!')
+
+
+# + - / *
+
+# ############################################ #
+
+# tamrine hafteye baaad tabdil kardane proje ghabli ba function nevisi
+
+# def check_numbers(a):
+#     if a == 'hi':
+#         return True, 'hi', 10
+#     else:
+#         return False, 'hello', 0
+#
+#
+# a, b, c = check_numbers('hi')
+# print(a)
+# print(b)
+# print(c)
+
+
+# def func2(a):
+#     c = 10
+#     return 250
+#
+# asdasdasd = func2(65465465465)
+# print(asdasdasd)
+
+# ################################## #
+# print(sum([12, 45, 66, 77, 88]))
+#
+# print(max([12, 45, 66, 77, 88]))
+#
+# print(min([12, 45, 66, 77, 88]))
+#
+# print(min([12, 45, 66, 77, 88]))
+
+# def choice():
+#     return ''
+
+# turtle , math
+# import turtle
+# import random
+# b = [12, 45, 66, 77, 88]
+# a = random.choice(b)
+# random.shuffle(b)
+# print(a)
+# print(b)
+# function password
+# 10
+# a-z -> yes, No
+# A-Z
+# 0-1
+
+# 45612
+
