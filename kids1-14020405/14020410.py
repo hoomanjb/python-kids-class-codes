9e748653b5e941fd868f88d13bd593c4720d800a
hoomanjb <hoomanjavan74@gmail.com>
2023-07-01 20:30:00 +0400

    add 14020410.py

---
+++ kids1-14020405/14020410.py
@@ -0,0 +1,116 @@
+first_name = 'Anisa'
+# Reserved Word: print - in - or - and - my_print - if - for - while - class
+
+# print(5 * 88)
+# print = 10
+# print(5 * 70)
+
+# ###################################################
+# a = -10
+# b = 5
+# c = a + b
+# print(c)
+
+
+# ############################# Number
+# Integer - int -- Float
+
+# a = 52
+# b = -52
+# c = type(a)
+# print(c)
+# print(type(b))
+#
+# d = 5.9
+# e = -8.65465
+# print(type(d))
+# print(type(e))
+
+# a = 3j + 15
+# print(type(a))
+
+a = 10
+b = 3
+
+# c = a + b
+# d = a - b
+# e = a / b  # 3.33333
+# f = a * b
+# g = a // b  # 3
+# h = a % b  # 1
+# i = a ** b  # 1000  -> tavan
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+# print(i)
+
+# حساب کنید بزرگترین عددی که سیستمتون میتونه حساب کنه یا ذخیره کنه با پایتون
+
+# print(32163123**6848)
+# ################### Boolean - bool
+
+# 1 True 's' ' ' [1]
+# 0 False  '' []
+
+# a = ''
+# print(bool(a))
+
+
+# a = 10
+# a = a + 5
+# a += 10  # a = a + 5
+# a /= 5
+# a = a * 6
+# a *= 6  # a = a * 6
+
+# operator = +-/*
+
+# a = 10
+# b = 7
+# print(a < b)
+# print(a > b)
+# print('a' > 'b')
+# print(a >= b)  # بزرگتر مساوی
+# print(a <= b)  # کوچکتر مساوری
+# print(a == b)   # برابری
+# print(a != b)  # نامساوی
+
+# a = 10
+# b = 10
+# print(a >= b)
+# print(a > b or a == b)
+
+
+# and - or
+# a = 8
+# b = 9
+# print(a < b)
+# print(a < b and a > 10)
+# print(a < b or a > 10)
+
+# And
+# 0 and 0 = 0
+# 0 and 1 = 0
+# 1 and 0 = 0
+# 1 and 1 = 1
+
+# or
+# 0 or 0 = 0
+# 0 or 1 = 1
+# 1 or 0 = 1
+# 1 or 1 = 1
+
+a = 15
+b = 24
+c = a + (b - a) * 5 + 7 / a + (15 ** 2)
+print(c)
+# olaviat () ** * / - +
+y = b - a
+d = 15 ** 2
+e = 7 / a
+f = y * 5
+x = a + f + e + d
+print(x)
