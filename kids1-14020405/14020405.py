f04195907a0f55107275501245ca693b26a3c3db
hoomanjb <hoomanjavan74@gmail.com>
2023-07-01 18:36:59 +0400

    add 14020405.py

---
+++ kids1-14020405/14020405.py
@@ -0,0 +1,35 @@
+# print(9*45)
+
+# x = 5
+# y = 10
+#
+# z = x + y
+#
+# print(z)
+
+# ############## # Variable
+# a = 10
+# 1- hamegi horoof koochik
+# 2- snake case
+# 3- maana dar
+# first_name = 'Anisa'
+# age = 45
+# emai_address = 'test@gmail.com'
+# Name = 'ali'
+# # special char: !@#$%^&*()+-/?><
+# last_name = '!@#$%^&*()+-/?><'
+#
+# a = 10
+# b = '10'
+# print(a + b)
+#
+# a = 'ali'
+# b = "ali's brother"
+# a = 6
+# if a < 5: {
+#     # code
+# }
+
+a = 10
+b = 20
+
