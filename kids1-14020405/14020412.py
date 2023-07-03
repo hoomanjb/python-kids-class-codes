37bdfba300dcf7a94899f9df9e037c8ec83ac537
hoomanjb <hoomanjavan74@gmail.com>
2023-07-03 20:28:29 +0400

    add 14020412.py

---
+++ kids1-14020405/14020412.py
@@ -0,0 +1,116 @@
+# a = 15
+# b = 24
+# c = a + (b - a) * 5 + (7 / a) + 15 ** 2
+# print(c)
+# # olaviat () ** * / - +
+# y = b - a
+# d = 15 ** 2
+# e = 7 / a
+# f = y * 5
+# x = a + f + e + d
+# print(x)
+# #########################################
+# String - Text
+
+# name = 'Anisa'
+# name = "Anisa"
+# nAme = 'anisa'
+# print(type(name))
+
+
+# a = 10.9
+# print(a)
+# b = int(a)
+# print(b)
+# c = float(b)
+# print(c)
+
+# import math
+# a = 10.1
+# print(math.ceil(a))
+# print(math.floor(a))
+# print(round(a))
+
+# a = 'salam'
+# print(a)
+# print(type(a))
+# b = int(a)
+# print(b)
+# print(type(b))
+
+
+# name = 'Anisa;aksm;dlkalksmkkskskksdjnflksj551120'
+# #       0123456789
+# print(name[0])
+# print(name[1])
+# print(name[4])
+# print(len(name))
+# last_index = len(name) - 1
+# print(name[last_index])
+
+
+email_address = 'test@ gmail. com 12312 %^#%^&@'
+# print(email_address[-1])
+# print(email_address[-2])
+# print(email_address[-3])
+# print(email_address[-4])
+# print(email_address[0:10])
+# print(email_address[5:17])
+# print(email_address[20:5])
+# print(email_address[:])
+# print(email_address[:15])
+# print(email_address[15:])
+# print(email_address[200])
+# Slicing name[start:end:step]
+# print(email_address[:15:3])
+
+# a = 'salam'
+# print(a)
+# print(a[::4])
+# # malas
+# print(a[::-1])
+# print(a[::-2])
+
+# first_name = 'hooman'
+# last_name = 'javanbakht'
+#
+# result = first_name + ' ' + last_name
+# print(result)
+
+# a = '**--**--'
+# print(50 * a)
+# a = 'hello'
+# b = ' '
+# c = 'world'
+# a, b, c = 'hello', '1', 'world'
+# print(a, b, c)
+
+# name = 'anisa'
+# name = input('enter your name: ')
+# print('esme shoma: ', name)
+
+# number1 = int(input('enter your first number: '))
+# number2 = int(input('enter your second number: '))
+#
+# print(type(number1))
+# print(type(number2))
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 / number2)
+# print(number1 * number2)
+
+# in , not in
+# variable = '113465'
+# print('a' in variable)
+# print('a' not in variable)
+
+# while True:
+#     number1 = input('enter your first number: ')
+#     if number1.isdigit():
+#         print('ok')
+#         break
+#     else:
+#         print('wrong')
+
+a = 'test@gmail.com'
+print('.com' in a)
