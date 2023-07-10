788716aae411d96af1f08a5246b56cf4fb1e68d4
hoomanjb <hoomanjavan74@gmail.com>
2023-07-10 20:29:47 +0400

    add 14020419.py

---
+++ kids1-14020405/14020419.py
@@ -0,0 +1,109 @@
+
+# age = '15 - 25'
+# elif age in ['15', '16', '17', '18']
+# elif age == '15 - 25':
+#     print('I hope you know that your the main character')
+# #########################################################
+import math
+
+# a = 10.6
+# print(round(a))
+# print(math.floor(a))
+
+import turtle
+
+turtle.getscreen()
+t = turtle.Turtle()
+turtle.title('Anisa Class')
+turtle.bgcolor('yellow')
+t.speed(1)
+
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# # t.left(90)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+
+
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(135)
+# t.forward(100)
+
+# t.forward(100)
+# t.left(120)
+# t.forward(100)
+# t.left(120)
+# t.forward(100)
+
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+# t.forward(100)
+# t.right(60)
+
+t.pensize(4)
+t.shapesize(2)
+t.color('red')
+t.pencolor('green')
+# t.circle(-100)
+
+# t.begin_fill()
+# t.color('blue')
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.end_fill()
+
+# t.fd()
+# t.rt()
+# t.lt()
+# t.bk()
+
+
+# t.goto(100, 0)
+# t.goto(100, 100)
+# t.goto(0, 100)
+# t.goto(0, 0)
+# t.home()
+# t.penup()
+# t.goto(250, 100)
+# t.pendown()
+
+
+t.goto(100,0)
+t.goto(100,100)
+t.goto(0,100)
+t.home()
+t.penup()
+t.goto(100,100)
+t.pendown()
+t.goto(50,50)
+t.goto(0,100)
+
+# t.dot(100)
+
+
+turtle.mainloop()
+
+# a = print()
+# b = print
+#
+# print(a)
+# print(b)
+# b('salam')
